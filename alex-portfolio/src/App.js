956d9ccc550ed278c9468e001ea6c38a370ed6db
Alex <alexmillerprojects@gmail.com>
2020-07-02 12:23:16 -0700

    rebuilding portfolio site

---
+++ alex-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Portfolio</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
