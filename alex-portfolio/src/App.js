8cbe5b5fbf269bc2456ec146f3647c0b2de94727
Alex <alexmillerprojects@gmail.com>
2020-08-11 14:12:06 -0700

    added new layout for portfolio with react-app

--- alex-portfolio/src/App.js
+++ alex-portfolio/src/App.js
@@ -5,13 +5,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='hero'>
       <header>
-        <nav>
-          <a href="#">Home</a>
-          <a href="#">Portfolio</a>
-          <a href="#">Contact</a>
+        <nav className='navbar'>
+          <div className='logo'>
+            <h2>Made By Alex</h2>
+          </div>
+          
+          <div className='links'>
+            <a href="#">Home</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Contact</a>
+          </div>
         </nav>
       </header>
+        <div className='container'>
+      
+          <div className='port-item'>
+            <img src='https://gdurl.com/9loK/'/>
+            <div className='port-desc'>
+              <h3>PORTFOLIO TITLE</h3>
+              <p>PORTFOLIO DESCRIPTION LOREM IPSUM</p>
+            </div>
+          </div>
+
+      </div>
+      </div>
     </div>
   );
 }
