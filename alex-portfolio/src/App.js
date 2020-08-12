010cb70f1613a3c43dacccfce86e4d1ddf197b81
Alex <alexmillerprojects@gmail.com>
2020-08-12 14:49:27 -0700

    be and fe working. working on styling now

--- alex-portfolio/src/App.js
+++ alex-portfolio/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/projects').then(res => {
+      // console.log(res)
+      setProjects(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  console.log(projects)
+
   return (
     <div className="App">
       <div className='hero'>
@@ -20,18 +33,23 @@ function App() {
         </nav>
       </header>
         <div className='container'>
-      
+
+        {projects.map(e => {
+          return (
           <div className='port-item'>
-            <img src='https://gdurl.com/9loK/'/>
+            <img src={e.proj_img}/>
             <div className='port-desc'>
-              <h3>PORTFOLIO TITLE</h3>
-              <p>PORTFOLIO DESCRIPTION LOREM IPSUM</p>
+              <h3>{e.proj_name}</h3>
+              <p>{e.desc}</p>
             </div>
+          </div>
+          )
+        })}
+
           </div>
 
       </div>
       </div>
-    </div>
   );
 }
 
