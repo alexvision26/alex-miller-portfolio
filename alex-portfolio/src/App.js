566fe75ee0eedcd06dd62f2435ce6ed1c6d930b2
Alex <alexmillerprojects@gmail.com>
2020-08-14 14:08:58 -0700

    portfolio structure in place. working on styling and contact page

--- alex-portfolio/src/App.js
+++ alex-portfolio/src/App.js
@@ -1,6 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Hero from './components/hero';
 
 function App() {
   const [projects, setProjects] = useState([])
@@ -21,17 +29,21 @@ function App() {
       <div className='hero'>
       <header>
         <nav className='navbar'>
-          <div className='logo'>
-            <h2>Made By Alex</h2>
-          </div>
-          
-          <div className='links'>
-            <a href="#">Home</a>
-            <a href="#">Portfolio</a>
-            <a href="#">Contact</a>
+          <div className='nav-content'>
+            <div className='logo'>
+              <h2>Made By Alex</h2>
+            </div>
+            
+            <div className='links'>
+              <a href="#">Home</a>
+              <a href="#projects">Portfolio</a>
+              <a href="#">Contact</a>
+            </div>
           </div>
         </nav>
       </header>
+      <Hero/>
+      <h2 id='projects' className='proj-sec'>Projects</h2>
         <div className='container'>
 
         {projects.map(e => {
@@ -41,11 +53,26 @@ function App() {
             <div className='port-desc'>
               <h3>{e.proj_name}</h3>
               <p>{e.desc}</p>
+              <div className='proj-buttons'>
+                <button>View App</button>
+                <button>View Code</button>
+              </div>
             </div>
           </div>
           )
         })}
 
+          </div>
+          <div className='contact-sec'>
+            <h2>Ready to work?</h2>
+            <button className='cta-button'>Let's talk!</button>
+          </div>
+
+          <div className='footer-sec'>
+            <h3>Alex Miller | Full Stack Software Developer | 2020</h3>
+            <a href="#">> Home</a>
+            <a href="#">> Portfolio</a>
+            <a href="#">> Contact</a>
           </div>
 
       </div>
---
+++ alex-portfolio/src/components/hero.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './hero.css'
+
+function Hero() {
+
+    return(
+        <>
+        <div className='hero-banner'>
+            <div className='overlay'>
+                <div className='hero-content'>
+                    <h1 className='title'>Alex Miller</h1>
+                    <h2 className='subtitle'>Full Stack Software Developer</h2>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Hero;
